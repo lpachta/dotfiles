da9516fac521c5407e34a4efb3a94cad0a329643
lpachta <133801351+lpachta@users.noreply.github.com>
2025-06-01 09:23:21 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@ sudo pacman -S kitty ttf-jetbrains-mono-nerd zsh fzf zoxide nvim eza fastfetch
 echo ""
 
 echo "🔗 Linking dotfiles..."
-mv ~/.zshrc{,.bak}
+mv -T ~/.zshrc{,.bak}
 ln -sf $HOME/dotfiles/.zshrc $HOME/.zshrc
-mv ~/.config/zshrc{,.bak}
-ln -sf $HOME/dotfiles/.config/zshrc/ $HOME/.config/zshrc/
-mv ~/.config/kitty{,.bak}
-ln -sf $HOME/dotfiles/.config/kitty/ $HOME/.config/kitty/
+mv -T ~/.config/zshrc{,.bak}
+ln -sf $HOME/dotfiles/.config/zshrc $HOME/.config/zshrc
+mv -T ~/.config/kitty{,.bak}
+ln -sf $HOME/dotfiles/.config/kitty $HOME/.config/kitty
 echo ""
 
 echo "💤 Installing LazyVim..."
-mv ~/.config/nvim{,.bak}
-mv ~/.local/share/nvim{,.bak}
-mv ~/.local/state/nvim{,.bak}
-mv ~/.cache/nvim{,.bak}
+mv -T ~/.config/nvim{,.bak}
+mv -T ~/.local/share/nvim{,.bak}
+mv -T ~/.local/state/nvim{,.bak}
+mv -T ~/.cache/nvim{,.bak}
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 ln -sf $HOME/dotfiles/.config/nvim/lua/plugins/transparent.lua $HOME/.config/nvim/lua/plugins/transparent.lua
